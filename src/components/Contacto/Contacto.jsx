e5976976a9bcba753a02b7867467ea7595e51656
joaquingutierrez <joaquinguty@gmail.com>
2023-02-20 20:01:18 -0300

    Arreglando css de preguntas frecuentes y Contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,6 +1,10 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AnchorsContext } from "../../context/anchorsContext";
 import "./style.css"
+import imagenMapa from "../../assets/mapa.png"
+import clockLogo from "../../assets/Clock.png"
+import whatsappLogo from "../../assets/WhatsApp.png"
+import envelopeLogo from "../../assets/Envelope.png"
 
 function Contacto() {
 
@@ -9,20 +13,37 @@ function Contacto() {
   useEffect(() => {
     const anchorContacto = document.getElementById("contactoContainer")
     allAnchors.push({ name: "CONTACTO", anchor: anchorContacto })
-}, [])
+  }, [])
 
 
   return (
     <div id="contactoContainer">
       <h2 className="tituloContacto">Contacto</h2>
-      <div className="rectangulo">
+      <div className="rectanguloContacto">
+        <div className='contenidoDeContacto'>
+          <div className='imagenContacto'>
+            <img src={imagenMapa} alt="" />
+          </div>
+          <div className='cuerpoContacto'>
+            <h3 className="tituloUbicacion">Sede Bella Vista</h3>
+            <div className="logoYTextoflex">
+              <img src={clockLogo} alt="" />
+              <p className="horarioContacto">Horario: todos los días de 8 a 20 hs</p>
+            </div>
+            <div className="logoYTextoflex">
+              <img src={whatsappLogo} alt="" />
+              <p className="WhatsappContacto"> Whatsapp: +54 11 6985-3215</p>
+            </div>
+            <div className="logoYTextoflex">
+              <img src={envelopeLogo} alt="" />
+              <p className="EmailContacto">Email: espacioklem@gmail.com</p>
+            </div>
+            <p className="asistenciaContacto"> La asistencia es únicamente con turno.</p>
+            <p className="asistenciaContacto">En caso de querer cancelar el turno, por favor, avisanos con anticipación.</p>
 
-        <h3 className="titulo">Sede Bella Vista</h3>
-        <p className="horario">Horario: todos los días de 8 a 20 hs</p>
-        <p className="Whatsapp"> Whatsapp: +54 11 6985-3215</p>
-        <p className="Email">Email: espacioklem@gmail.com</p>
-        <p className="asistencia"> La asistencia es únicamente con turno.
-          En caso de querer cancelar el turno, por favor, avisanos con anticipación.</p>
+          </div>
+
+        </div>
 
       </div>
     </div>
--- src/components/PreguntasFrecuentes/PreguntasFrecuentes.jsx
+++ src/components/PreguntasFrecuentes/PreguntasFrecuentes.jsx
@@ -1,6 +1,8 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AnchorsContext } from "../../context/anchorsContext";
 import "./style.css"
+import preguntaLogo from "../../assets/help-circle.png"
+
 
 function PreguntasFrecuentes() {
 
@@ -9,43 +11,69 @@ function PreguntasFrecuentes() {
   useEffect(() => {
     const anchorPreguntasFrecuentes = document.getElementById("preguntasFrecuentesContainer")
     allAnchors.push({ name: "PREGUNTAS FRECUENTES", anchor: anchorPreguntasFrecuentes })
-}, [])
+  }, [])
 
   return (
     <div id="preguntasFrecuentesContainer">
       <a className="anchor" id="preguntasFrecuentes"></a>
       <h2 className="tituloPreguntas">Preguntas Frecuentes</h2>
-      <div className="container overflow-hidden text-center">
-        <div className="row gy-5">
-          <div className="col-6">
-            <div className="p-3">¿Cómo sé qué tipo de masaje es mejor para mí?</div>
-            <p className="texto">Esto depende de tus necesidades específicas. Si estás buscando aliviar el dolor muscular o una tensión crónica, entonces un masaje descontracturante sería una mejor opción. Si buscas relajarte y disfrutar de un masaje para tus músculos, entonces un masaje relajante sería una mejor opción. Si estás en duda, consulta a un profesional para obtener la mejor recomendación.</p>
+
+
+      <div className="seccionPreguntas">
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Cómo sé qué tipo de masaje es mejor para mí?</h3>
           </div>
-          <div className="col-6">
-            <div className="p-3">¿Qué productos utilizan en las sesiones de masajes?</div>
-            <p className="texto">Los productos que se utilizan en las sesiones de masajes dependen de los objetivos buscados y tipo de masaje. Los aceites esenciales se utilizan por a sus propiedades curativas y relajantes. Otros productos que se pueden utilizar son cremas y geles, envolturas calientes o frías, toallas térmicas y almohadas para masajes.</p>
+          <p className="texto">Esto depende de tus necesidades específicas. Si estás buscando aliviar el dolor muscular o una tensión crónica, entonces un masaje descontracturante sería una mejor opción. Si buscas relajarte y disfrutar de un masaje para tus músculos, entonces un masaje relajante sería una mejor opción. Si estás en duda, consulta a un profesional para obtener la mejor recomendación.</p>
+        </div>
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Qué productos utilizan en las sesiones de masajes?</h3>
           </div>
-          <div className="col-6">
-            <div className="p-3">¿Cuánto dura el lifting de pestañas?</div>
-            <p className="texto">La duración depende mucho del ciclo de crecimiento de tu pelo. El mismo está determinado por factores y hábitos que varían según la persona. Factores como el cuidado y peinado de las pestañas también van a favorecer que el resultado dure mucho más tiempo.
-              El tiempo estimado es de 4 a 8 semanas.</p>
-          </div>
-          <div className="col-6">
-            <div className="p-3">¿Cuánto dura el laminado de cejas?</div>
-            <p className="texto">La duración depende mucho del ciclo de crecimiento de tu pelo. El mismo está determinado por factores y hábitos que varían según la persona. Factores como el cuidado y peinado de las pestañas también van a favorecer que el resultado dure mucho más tiempo.
-              El tiempo estimado es de 2 a 6 semanas.</p>
-          </div>
-          <div className="col-6">
-            <div className="p-3">¿Qué métodos de pago aceptan?</div>
-            <p className="texto">Aceptamos todos los medios de pago, efectivo, transferencia, tarjeta de crédito y débito.</p>
-          </div>
-          <div className="col-6">
-            <div className="p-3">¿Qué debo tener en cuenta antes de asistir a mi turno?</div>
-            <p className="texto">No aplicar maquillajes, cremas o aceites en la zona a tratar 24 hs antes del turno.</p>
-          </div>
+          <p className="texto">Los productos que se utilizan en las sesiones de masajes dependen de los objetivos buscados y tipo de masaje. Los aceites esenciales se utilizan por a sus propiedades curativas y relajantes. Otros productos que se pueden utilizar son cremas y geles, envolturas calientes o frías, toallas térmicas y almohadas para masajes.</p>
         </div>
       </div>
 
+      <div className="seccionPreguntas">
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Cuánto dura el lifting de pestañas?</h3>
+          </div>
+          <p className="texto">La duración depende mucho del ciclo de crecimiento de tu pelo. El mismo está determinado por factores y hábitos que varían según la persona. Factores como el cuidado y peinado de las pestañas también van a favorecer que el resultado dure mucho más tiempo.</p>
+          <p className="texto">El tiempo estimado es de 4 a 8 semanas.</p>
+        </div>
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Cuánto dura el laminado de cejas?</h3>
+          </div>
+          <p className="texto">La duración depende mucho del ciclo de crecimiento de tu pelo. El mismo está determinado por factores y hábitos que varían según la persona. Factores como el cuidado y peinado de las pestañas también van a favorecer que el resultado dure mucho más tiempo.</p>
+            <p className="texto">El tiempo estimado es de 2 a 6 semanas.</p>
+
+        </div>
+
+      </div>
+
+      <div className="seccionPreguntas">
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Qué métodos de pago aceptan?</h3>
+          </div>
+          <p className="texto">Aceptamos todos los medios de pago, efectivo, transferencia, tarjeta de crédito y débito.</p>
+        </div>
+        <div>
+          <div className='logoMasPregunta'>
+            <img src={preguntaLogo} alt="" />
+            <h3 className="tituloPregunta">¿Qué debo tener en cuenta antes de asistir a mi turno?</h3>
+          </div>
+          <p className="texto">No aplicar maquillajes, cremas o aceites en la zona a tratar 24 hs antes del turno.</p>
+        </div>
+
+      </div>
     </div>
   )
 }
