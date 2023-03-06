e0de5d776cbeabbc831644169e8fb9d108f17a69
joaquingutierrez <joaquinguty@gmail.com>
2023-03-06 10:29:02 -0300

    Ahora todas las cards tienen las mismas medidas

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -21,32 +21,42 @@ const Servicios = () => {
     const card1 = {
         imagen: imagenMasajesRelajantes,
         descripcionImagen: "Imagen de alguien masajeando el cuello de otra persona",
-        titulo: "Masajes relajantes",
-        parrafo: "Utilizamos técnicas suaves y relajantes, como la aplicación de presión ligera y movimientos lentos y fluidos, con el objetivo de liberar la tensión acumulada en los músculos y mejorar el bienestar general."
+        titulo1: "Masajes",
+        titulo2: "relajantes",
+        parrafo: "Utilizamos técnicas suaves y relajantes, como la aplicación de presión ligera y movimientos lentos y fluidos, con el objetivo de liberar la tensión acumulada en los músculos y mejorar el bienestar general.",
+        alturaParrafo: "alturaParrafoMasajes"
     }
     const card2 = {
         imagen: imagenMasajesDescontracturantes,
         descripcionImagen: "Imagen de alguien masajeando la espalda de otra persona",
-        titulo: "Masajes descontracturantes",
-        parrafo: "Utilizamos técnicas de presión profunda, estiramiento y fricción, enfocadas en aliviar la tensión muscular, la rigidez en el cuerpo y reducir el dolor e inflamación."
+        titulo1: "Masajes",
+        titulo2: "descontracturantes",
+        parrafo: "Utilizamos técnicas de presión profunda, estiramiento y fricción, enfocadas en aliviar la tensión muscular, la rigidez en el cuerpo y reducir el dolor e inflamación.",
+        alturaParrafo: "alturaParrafoMasajes"
     }
     const card3 = {
         imagen: imagenLiftingDePestañas,
         descripcionImagen: "Imagen de alguien haciendole un lifting de pestañas a otra persona",
-        titulo: "Lifting de pestañas",
-        parrafo: "Arqueamos y levantamos tus propias pestañas, creando un efecto abanico súper natural, utilizando productos nutritivos para favorecer su crecimiento. Siguiendo los cuidados y recomendaciones, el efecto lifting puede durar de 4 a 8 semanas."
+        titulo1: "Lifting de",
+        titulo2: "pestañas",
+        parrafo: "Arqueamos y levantamos tus propias pestañas, creando un efecto abanico súper natural, utilizando productos nutritivos para favorecer su crecimiento. Siguiendo los cuidados y recomendaciones, el efecto lifting puede durar de 4 a 8 semanas.",
+        alturaParrafo: "alturaParrafoBelleza"
     }
     const card4 = {
         imagen: imagenDiseñoYPerfiladoDeCejas,
         descripcionImagen: "Imagen de alguien haciendole un perfilado de cejas a otra persona",
-        titulo: "Diseño y perfilado de cejas",
-        parrafo: "El perfilado de cejas es un procedimiento de belleza que tiene como objetivo dar forma y definición a las cejas, resaltando la estructura natural de la cara y mejorando la apariencia estética de las cejas. El proceso de perfilado de cejas implica la eliminación cuidadosa de los vellos no deseados para crear una forma simétrica y armoniosa."
+        titulo1: "Diseño y perfilado",
+        titulo2: "de cejas", 
+        parrafo: "El perfilado de cejas es un procedimiento de belleza que tiene como objetivo dar forma y definición a las cejas, resaltando la estructura natural de la cara y mejorando la apariencia estética de las cejas. El proceso de perfilado de cejas implica la eliminación cuidadosa de los vellos no deseados para crear una forma simétrica y armoniosa.",
+        alturaParrafo: "alturaParrafoBelleza"
     }
     const card5 = {
         imagen: imagenLaminadoDeCejas,
         descripcionImagen: "Imagen de alguien haciendole un laminado de cejas a otra persona",
-        titulo: "Laminado de cejas",
-        parrafo: "Consiste en aplicar un líquido que suaviza los vellos y los peina en una dirección específica. El resultado es una apariencia más pulida y definida de las cejas. Este tratamiento no es permanente, puede durar hasta 6 semanas, dependiendo del tipo de cabello y del cuidado posterior."
+        titulo1: "Laminado de",
+        titulo2: "cejas",
+        parrafo: "Consiste en aplicar un líquido que suaviza los vellos y los peina en una dirección específica. El resultado es una apariencia más pulida y definida de las cejas. Este tratamiento no es permanente, puede durar hasta 6 semanas, dependiendo del tipo de cabello y del cuidado posterior.",
+        alturaParrafo: "alturaParrafoBelleza"
     }
 
     return (
--- src/components/Servicios/ServiciosCard/index.jsx
+++ src/components/Servicios/ServiciosCard/index.jsx
@@ -9,8 +9,8 @@ const ServiciosCards = ({ card }) => {
             <div className="contenedorImagenCard">
                 <img src={card.imagen} alt={card.descripcionImagen} />
             </div>
-            <h3 className="tituloCard">{card.titulo}</h3>
-            <p className="parrafoCard">{card.parrafo}</p>
+            <h3 className="tituloCard">{card.titulo1}<br></br>{card.titulo2}</h3>
+            <p className={card.alturaParrafo + " " + "parrafoCard"}>{card.parrafo}</p>
 
             <Button name="Reservar turno" />
 
